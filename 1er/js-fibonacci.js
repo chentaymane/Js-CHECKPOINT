b699b959da8e32c261f6f5b1a9ac4863bf63a569
achent <159848379+chentaymane@users.noreply.github.com>
2026-04-28 14:47:36 +0100

    Refactor fibonacci function implementation
    Refactor fibonacci function to use trait array for calculation.

--- 1er/js-fibonacci.js
+++ 1er/js-fibonacci.js
@@ -1,18 +1,25 @@
 function fibonacci(n) {
-var arr = []
-var count = 0
-    for (let i=0 ; i<=n ; i++) {
-       arr.push(i)
-    }
+  var arr = []
+  var trait = []
+  let first = true
 
-    while (arr.length > 0) {
-        arr.remove(arr[0])
-        count++
-        arr.indexOf
-    }   
-    return count
+  for (let i = 0; i <= n; i++) {
+    arr.push(i)
+  }
+
+  while (trait.length <= n) {
+    if (first) {
+      trait.push(0, 1) // start correctly
+      arr.splice(0, 2)
+      first = false
+    } else {
+      trait.push(trait[trait.length - 1] + trait[trait.length - 2])
+      arr.splice(0, 1)
+    }
+  }
+
+  return trait[n]
 }
 
-
-console.log(fibonacci(6));
-console.log(fibonacci(10));
+console.log(fibonacci(6));  // 8
+console.log(fibonacci(10)); // 55
